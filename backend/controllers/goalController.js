2b0304d67bf2eb63b4bad975cec1572e75d00fa2
acutajar <aleccutajar@gmail.com>
2022-03-09 15:34:36 -0500

    errorHandler middleware and additional route setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,30 +1,36 @@
+const asyncHandler = require("express-async-handler");
 //@desc     Get goals
 //@route    GET /api/goals
 //@access   private
-const getGoals = (req, res) => {
-  res.status(200).json({ message: "Get goals" });
-};
+const getGoals = asyncHandler(async (req, res) => {
+  res.status(200);
+  throw new Error("Please add a text field.");
+});
 
 //@desc     Set goals
 //@route    SET /api/goals
 //@access   private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400).json({ message: "Please add a valid text field" });
+  }
   res.status(200).json({ message: "Set goal" });
-};
+});
 
 //@desc     Update a goal
 //@route    PUT /api/goals/:id
 //@access   private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
 //@desc     Delete a goal
 //@route    DELETE /api/goals/:id
 //@access   private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Deleted goal ${req.params.id}` });
-};
+});
+
 module.exports = {
   getGoals,
   setGoal,
