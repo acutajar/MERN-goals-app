80809b62883cf0d24823b6b92f339da627f797bc
acutajar <aleccutajar@gmail.com>
2022-04-27 13:28:49 -0400

    added jwt auth to goal routes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,13 @@
 const asyncHandler = require("express-async-handler");
 
 const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 
 //@desc     Get goals
 //@route    GET /api/goals
 //@access   private
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  const goals = await Goal.find({ user: req.user.id });
   res.status(200).json(goals);
 });
 
@@ -17,7 +18,12 @@ const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400).json({ message: "Please add a valid text field" });
   }
-  const goal = await Goal.create({ text: req.body.text });
+  const goal = await Goal.create({
+    text: req.body.text,
+    user: req.user.id,
+    name: "hello",
+  });
+
   res.status(200).json(goal);
 });
 
@@ -29,6 +35,20 @@ const updateGoal = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error("Goal not found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  // Check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure logged in user matches the goal's user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -44,6 +64,20 @@ const deleteGoal = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error("Goal not found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  // Check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure logged in user matches the goal's user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
   await goal.remove();
 
